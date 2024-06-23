f3573dcc5fc3ddc5bbf1485bb9658450f99dec3c
nayem567 <inew2732@gmail.com>
2024-06-24 00:56:14 +0600

    CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
     "./index.html",
-    "./*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     screens: {
@@ -17,13 +17,12 @@ module.exports = {
         brightRedLight: 'hsl(12, 88%, 69%)',
         brightRedSupLight: 'hsl(12, 88%, 95%)',
         darkBlue: 'hsl(228, 39%, 23%)',
-        darkGrayishBlue: 'hsl (227, 12%, 61%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
-        veryLightGray: 'hsl(0, 0%, 98%)', 
-      }
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      },
     },
-
   },
   plugins: [],
-}
+};
